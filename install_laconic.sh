4de865ab2016c195aa649e55da5cd9321b3bbaaa
0xzhara.eth <122523974+0xzhara@users.noreply.github.com>
2024-08-14 15:45:00 +0700

    Create install_laconic.sh

---
+++ install_laconic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Display logo and message
+curl -s https://raw.githubusercontent.com/zrhraJETTOKOSUTA/bash-nobi.sh/main/bash%20logo.sh | bash
+echo "Join the Airdrop Nobi Telegram channel: https://t.me/airdropnobi"
+sleep 4
+
+# Update package lists
+sudo apt update
+
+# Install curl
+sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+
+# Install go
+cd $HOME
+VER="1.22.2"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+echo $(go version) && sleep 1
